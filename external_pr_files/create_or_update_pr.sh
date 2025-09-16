9d1ba50ecf5b3dce3ff0dc77e7dd5813dc0f7c5d
QuietHellsPage <baazhinga@yandex.ru>
2025-09-16 19:57:27 +0300

    hf

--- external_pr_files/create_or_update_pr.sh
+++ external_pr_files/create_or_update_pr.sh
@@ -8,17 +8,9 @@ BRANCH_NAME="auto-update-from-$REPO_NAME-pr-$SOURCE_PR_NUMBER"
 
 cd $TARGET_REPO
 
-# COMMENT_BODY="$COMMENT_BODY:-'No comment body available'"
-
 TARGET_PR_NUMBER=$(gh pr list --repo QuietHellsPage/$TARGET_REPO --head $BRANCH_NAME --json number -q '.[0].number' 2>/dev/null || true)
 
 if [ -z "$TARGET_PR_NUMBER" ]; then
-#     PR_BODY="Automated sync from $REPO_NAME PR $SOURCE_PR_NUMBER
-# Source PR: $SOURCE_PR_NUMBER
-# Commit: $GITHUB_SHA
-# Triggered by: $GITHUB_ACTOR
-# Comment: $COMMENT_BODY"
-    
     gh pr create \
         --repo QuietHellsPage/$TARGET_REPO \
         --head $BRANCH_NAME \
